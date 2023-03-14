663465f5fff86416dae0cb86b3646b4dba983d37
Madara Gaiden <75212705+madara-gaiden@users.noreply.github.com>
2023-03-14 01:28:05 +0100

    Add files via upload

---
+++ 1-Introduction And Variables.py
@@ -0,0 +1,39 @@
+# ----------------------------
+#       Assignment 1
+# Created to answer Assignments 1 to 5
+# Created By Madara Gaiden
+# ----------------------------
+
+# ----------------------------
+#       Assignment 2
+# ----------------------------
+
+Name = "Madara"
+Age = "120"
+Country = "Konoha"
+
+# ----------------------------
+#       Assignment 3
+print("="*90)
+# ----------------------------
+
+print("\"Name: " + Name + "\"")
+print("\"Age: " + Age + "\"")
+print("\"Country: " + Country + "\"")
+
+# ----------------------------
+#       Assignment 4
+print("="*90)
+# ----------------------------
+
+print("Hello, My Name Is " + Name + " And Iam " +
+      Age + " Years Old and I Live in " + Country + ".")
+ 
+# ----------------------------
+#       Assignment 5
+print("="*90)
+# ----------------------------
+
+print(type(Name))
+print(type(Age))
+print(type(Country))
