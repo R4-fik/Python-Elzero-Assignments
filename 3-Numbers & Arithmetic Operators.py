f7ff11b2d7c65bc1b122150cf2072674f8a36a48
Madara Gaiden <75212705+madara-gaiden@users.noreply.github.com>
2023-06-10 14:14:56 +0100

    Add files via upload

---
+++ 3-Numbers & Arithmetic Operators.py
@@ -0,0 +1,43 @@
+# ----------------------------
+#       Assignment 1
+# ----------------------------
+
+print(19)  # Integer
+print(19.5)  # Float
+print(19+5j)  # Complex
+
+# ----------------------------
+#       Assignment 2
+print("="*90)
+# ----------------------------
+
+print(f"Imaginary Part Is : {1+2j.imag}")
+print(f"Real Part Is : {1+2j.real}")
+
+# ----------------------------
+#       Assignment 3
+print("="*90)
+# ----------------------------
+
+num = 10
+print("%.10f" % num)
+
+# ----------------------------
+#       Assignment 4
+print("="*90)
+# ----------------------------
+
+num = 159.650
+print(int(num))
+print(type(int(num)))
+
+# ----------------------------
+#       Assignment 5
+print("="*90)
+# ----------------------------
+
+print(100 - 115)
+print(50 * 30)
+print(21 % 4)
+print(110 // 11)
+print(97 // 20)
